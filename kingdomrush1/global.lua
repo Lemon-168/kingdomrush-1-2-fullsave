655d3b15b1dea32885bc389dccf1a7bed3049462
Liu Linlin <15081209682@163.com>
2026-07-03 14:41:40 +0800

    Add files via upload

---
+++ kingdomrush1/global.lua
@@ -0,0 +1,7 @@
+local obj1 = {
+	["first_launch_time"] = 1737455392;
+	["marketing"] = {
+		["session_count"] = 14;
+	};
+}
+return obj1
---
+++ kingdomrush1/slot_1.lua
@@ -0,0 +1,435 @@
+local obj1 = {
+	["achievement_counters"] = {
+		["ACDC"] = 4925;
+		["ARMY_OF_ONE"] = 6;
+		["AXE_RAINER"] = 743;
+		["BEAM_ME_UP"] = 510;
+		["BLOODLUST"] = 24942;
+		["CANNON_FODDER"] = 4755;
+		["CLUSTERED"] = 872;
+		["DEATH_FROM_ABOVE"] = 1458;
+		["DEFEAT_COOL_RUNNING"] = 37;
+		["DEFEAT_MUSHROOM"] = 31;
+		["DIE_HARD"] = 263927;
+		["DUST_TO_DUST"] = 195;
+		["EARN15_STARS"] = 78;
+		["EARN30_STARS"] = 78;
+		["EARN45_STARS"] = 78;
+		["EASY_TOWER_BUILDER"] = 473;
+		["ENTANGLED"] = 712;
+		["FIRST_BLOOD"] = 24942;
+		["GI_JOE"] = 2378;
+		["HARD_TOWER_BUILDER"] = 473;
+		["HERO_HARD"] = 10;
+		["HERO_MEDIUM"] = 10;
+		["HOLY_CHORUS"] = 356;
+		["MEDIC"] = 52168;
+		["MEDIUM_TOWER_BUILDER"] = 473;
+		["MULTIKILL"] = 24942;
+		["RATATOUILLE"] = 84;
+		["REAL_STATE"] = 7;
+		["ROCKETEER"] = 195;
+		["SHEEP_KILLER"] = 1;
+		["SHEPARD"] = 24;
+		["SLAYER"] = 24942;
+		["SNIPER"] = 42;
+		["SPECIALIZATION"] = 255;
+		["SPORE"] = 107;
+		["STILL_COUNTS_AS_ONE"] = 27447;
+		["SUN_BURNER"] = 48;
+		["TACTICIAN"] = 714;
+		["TOXICITY"] = 63;
+		["UPGRADE_LEVEL3"] = 15;
+		["WE_DINE_IN_HELL"] = 1400;
+		["WHATS_THAT"] = 41;
+	};
+	["achievements"] = {
+		["ACDC"] = true;
+		["ARMAGGEDON"] = true;
+		["AXE_RAINER"] = true;
+		["BEAM_ME_UP"] = true;
+		["BLOODLUST"] = true;
+		["CANNON_FODDER"] = true;
+		["CATCH_A_FISH"] = true;
+		["DARING"] = true;
+		["DEATH_FROM_ABOVE"] = true;
+		["DEFEAT_ACORN"] = true;
+		["DEFEAT_COMPLETE_HARD"] = true;
+		["DEFEAT_COOL_RUNNING"] = true;
+		["DEFEAT_END_BOSS"] = true;
+		["DEFEAT_GULTHAK_BOSS"] = true;
+		["DEFEAT_JUGGERNAUT"] = true;
+		["DEFEAT_KINGPING_BOSS"] = true;
+		["DEFEAT_MOUNTAIN_BOSS"] = true;
+		["DEFEAT_MUSHROOM"] = true;
+		["DEFEAT_SARELGAZ"] = true;
+		["DEFEAT_TREANT_BOSS"] = true;
+		["DEFEAT_TROLL_BOSS"] = true;
+		["DIE_HARD"] = true;
+		["DUST_TO_DUST"] = true;
+		["EARN15_STARS"] = true;
+		["EARN30_STARS"] = true;
+		["EARN45_STARS"] = true;
+		["EASY_TOWER_BUILDER"] = true;
+		["ENERGY_NETWORK"] = true;
+		["ENTANGLED"] = true;
+		["FEARLESS"] = true;
+		["FIRST_BLOOD"] = true;
+		["FREE_FREDO"] = true;
+		["GI_JOE"] = true;
+		["GOC"] = true;
+		["GREAT_DEFENDER"] = true;
+		["HARD_TOWER_BUILDER"] = true;
+		["HELL_O"] = true;
+		["HERO_HARD"] = true;
+		["HERO_MEDIUM"] = true;
+		["HOLY_CHORUS"] = true;
+		["IMPATIENT"] = true;
+		["MAX_ELVES"] = true;
+		["MEDIC"] = true;
+		["MEDIUM_TOWER_BUILDER"] = true;
+		["MULTIKILL"] = true;
+		["NEVERMORE"] = true;
+		["RATATOUILLE"] = true;
+		["ROCKETEER"] = true;
+		["SLAYER"] = true;
+		["SPECIALIZATION"] = true;
+		["SPLINTER"] = true;
+		["SPORE"] = true;
+		["STILL_COUNTS_AS_ONE"] = true;
+		["SUN_BURNER"] = true;
+		["SUPER_MUSHROOM"] = true;
+		["TACTICIAN"] = true;
+		["TOXICITY"] = true;
+		["UPGRADE_LEVEL3"] = true;
+		["WE_DINE_IN_HELL"] = true;
+		["WHATS_THAT"] = true;
+	};
+	["bag"] = {
+	};
+	["difficulty"] = 3;
+	["gems"] = 8133;
+	["heroes"] = {
+		["selected"] = "hero_elora";
+		["status"] = {
+			["hero_10yr"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_alleria"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_bolin"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_denas"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_elora"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_gerald"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_hacksaw"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_ignus"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_ingvar"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_magnus"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_malik"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_oni"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+			["hero_thor"] = {
+				["skills"] = {
+				};
+				["xp"] = 0;
+			};
+		};
+	};
+	["levels"] = {
+		[1] = {
+			[1] = 3;
+			[2] = 3;
+			[3] = 3;
+			["stars"] = 3;
+		};
+		[2] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[3] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[4] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[5] = {
+			[1] = 3;
+			[3] = 3;
+			["stars"] = 3;
+		};
+		[6] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[7] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[8] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[9] = {
+			[1] = 3;
+			[3] = 3;
+			["stars"] = 3;
+		};
+		[10] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[11] = {
+			[1] = 3;
+			["stars"] = 2;
+		};
+		[12] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[13] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[14] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[15] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[16] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[17] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[18] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[19] = {
+			[1] = 3;
+			["stars"] = 2;
+		};
+		[20] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[21] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[22] = {
+			[1] = 3;
+			["stars"] = 2;
+		};
+		[23] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[24] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[25] = {
+			[1] = 3;
+			["stars"] = 3;
+		};
+		[26] = {
+			[1] = 3;
+			["stars"] = 2;
+		};
+	};
+	["seen"] = {
+		["POWER_FIREBALL"] = true;
+		["POWER_REINFORCEMENT"] = true;
+		["TIP_ARMOR"] = true;
+		["TIP_ARMOR_HARD"] = true;
+		["TIP_ARMOR_MAGIC"] = true;
+		["TIP_ELITE"] = true;
+		["TIP_HEROES"] = true;
+		["TIP_RALLY"] = true;
+		["TIP_STRATEGY"] = true;
+		["TIP_UPGRADES"] = true;
+		["TOWER_BARBARIAN_BGF"] = true;
+		["TOWER_LEVEL2"] = true;
+		["TOWER_LEVEL3"] = true;
+		["TOWER_MUSKETEER"] = true;
+		["TOWER_PALADIN"] = true;
+		["TOWER_RANGER"] = true;
+		["TOWER_SORCERER"] = true;
+		["TOWER_TESLA"] = true;
+		["TUTORIAL_1"] = true;
+		["eb_blackburn"] = true;
+		["eb_greenmuck"] = true;
+		["eb_gulthak"] = true;
+		["eb_jt"] = true;
+		["eb_juggernaut"] = true;
+		["eb_kingpin"] = true;
+		["eb_moloch"] = true;
+		["eb_myconid"] = true;
+		["eb_sarelgaz"] = true;
+		["eb_ulgukhai"] = true;
+		["eb_veznan"] = true;
+		["enemy_abomination"] = true;
+		["enemy_bandit"] = true;
+		["enemy_brigand"] = true;
+		["enemy_dark_knight"] = true;
+		["enemy_demon"] = true;
+		["enemy_demon_cerberus"] = true;
+		["enemy_demon_flareon"] = true;
+		["enemy_demon_gulaemon"] = true;
+		["enemy_demon_imp"] = true;
+		["enemy_demon_legion"] = true;
+		["enemy_demon_mage"] = true;
+		["enemy_demon_wolf"] = true;
+		["enemy_fallen_knight"] = true;
+		["enemy_fat_orc"] = true;
+		["enemy_forest_troll"] = true;
+		["enemy_gargoyle"] = true;
+		["enemy_giant_rat"] = true;
+		["enemy_goblin"] = true;
+		["enemy_goblin_zapper"] = true;
+		["enemy_golem_head"] = true;
+		["enemy_halloween_zombie"] = true;
+		["enemy_lava_elemental"] = true;
+		["enemy_lycan"] = true;
+		["enemy_lycan_werewolf"] = true;
+		["enemy_marauder"] = true;
+		["enemy_necromancer"] = true;
+		["enemy_ogre"] = true;
+		["enemy_orc_armored"] = true;
+		["enemy_orc_rider"] = true;
+		["enemy_pillager"] = true;
+		["enemy_raider"] = true;
+		["enemy_rocketeer"] = true;
+		["enemy_rotten_lesser"] = true;
+		["enemy_rotten_tree"] = true;
+		["enemy_sarelgaz_small"] = true;
+		["enemy_shadow_archer"] = true;
+		["enemy_shaman"] = true;
+		["enemy_sheep_ground"] = true;
+		["enemy_skeleton"] = true;
+		["enemy_skeleton_big"] = true;
+		["enemy_skeleton_blackburn"] = true;
+		["enemy_skeleton_warrior"] = true;
+		["enemy_slayer"] = true;
+		["enemy_spectral_knight"] = true;
+		["enemy_spectral_knight_spawn"] = true;
+		["enemy_spider_big"] = true;
+		["enemy_spider_rotten"] = true;
+		["enemy_spider_rotten_tiny"] = true;
+		["enemy_spider_small"] = true;
+		["enemy_spider_tiny"] = true;
+		["enemy_swamp_thing"] = true;
+		["enemy_troll"] = true;
+		["enemy_troll_axe_thrower"] = true;
+		["enemy_troll_brute"] = true;
+		["enemy_troll_chieftain"] = true;
+		["enemy_troll_skater"] = true;
+		["enemy_wererat"] = true;
+		["enemy_werewolf"] = true;
+		["enemy_whitewolf"] = true;
+		["enemy_witch"] = true;
+		["enemy_wolf"] = true;
+		["enemy_wolf_small"] = true;
+		["enemy_yeti"] = true;
+		["enemy_zombie"] = true;
+		["enemy_zombie_blackburn"] = true;
+		["tower_arcane"] = true;
+		["tower_arcane_wizard"] = true;
+		["tower_archer_1"] = true;
+		["tower_archer_2"] = true;
+		["tower_archer_3"] = true;
+		["tower_barbarian"] = true;
+		["tower_barrack_1"] = true;
+		["tower_barrack_2"] = true;
+		["tower_barrack_3"] = true;
+		["tower_bfg"] = true;
+		["tower_bgf"] = true;
+		["tower_build_archer"] = true;
+		["tower_build_barrack"] = true;
+		["tower_build_engineer"] = true;
+		["tower_build_mage"] = true;
+		["tower_elf"] = true;
+		["tower_elf_holder"] = true;
+		["tower_engineer_1"] = true;
+		["tower_engineer_2"] = true;
+		["tower_engineer_3"] = true;
+		["tower_holder"] = true;
+		["tower_holder_blackburn"] = true;
+		["tower_holder_grass"] = true;
+		["tower_holder_snow"] = true;
+		["tower_holder_wasteland"] = true;
+		["tower_mage_1"] = true;
+		["tower_mage_2"] = true;
+		["tower_mage_3"] = true;
+		["tower_musketeer"] = true;
+		["tower_paladin"] = true;
+		["tower_paladins"] = true;
+		["tower_ranger"] = true;
+		["tower_sasquash_holder"] = true;
+		["tower_sorcerer"] = true;
+		["tower_sunray"] = true;
+		["tower_tesla"] = true;
+	};
+	["upgrades"] = {
+		["archers"] = 5;
+		["barracks"] = 5;
+		["engineers"] = 5;
+		["mages"] = 5;
+		["rain"] = 5;
+		["reinforcements"] = 5;
+	};
+	["version_string"] = "kr1-desktop-5.6.12";
+}
+return obj1
